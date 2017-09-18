cabe2ac7d64c8b2b2c3f3829ca5b53af73db9745
howCode <francis@howcode.org>
2017-09-18 02:10:59 +0100

    Added source code
    Added source code

---
+++ subs.py
@@ -0,0 +1,10 @@
+import urllib.request
+import json
+
+name=input("Enter username: ")
+key = "YOUR KEY GOES HERE"
+
+data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+name+"&key="+key).read()
+subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
+
+print(name + " has " + "{:,d}".format(int(subs)) + " subscribers!🎉")
